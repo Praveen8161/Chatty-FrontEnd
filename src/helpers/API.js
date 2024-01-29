a5c7c71e3e7763d7a3a36e36afb4cdc6bd30dd15
Praveen8161 <rbalu8161@gmail.com>
2024-01-29 22:37:47 +0530

    chor: API Changed

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,3 +1,3 @@
 // Server API
-// export const API = "https://chatty-backend-kmgy.onrender.com";
-export const API = "http://localhost:5000";
+export const API = "https://chatty-backend-kmgy.onrender.com";
+// export const API = "http://localhost:5000";
